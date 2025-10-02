7f42b0cd94cf381fa32f574e402bb6319180a095
Kai-Jennings <149123213+Kai-Jennings@users.noreply.github.com>
2025-10-03 09:40:03 +1000

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,9 @@ CLEAR = "cls" if os.name == "nt" else "clear"
 
 class Game:
     def __init__(self):
-        self.guesses = []
-        self.guess_count = 0
         self.max_guesses = 6
         self.valid_words = set()
-        self.valid_answers = set()
-        self.win = False
+        self.valid_answers = []
 
         with open("valid_words.txt", "r") as f:
             for line in f:
@@ -22,11 +19,12 @@ class Game:
 
         with open("valid_answers.txt", "r") as f:
             for line in f:
-                self.valid_answers.add(line.strip())
+                self.valid_answers.append(line.strip())
 
-        self.answer = random.choice(list(self.valid_answers))
-
-        self.game_loop()
+        self.answer = random.choice(self.valid_answers)
+        self.guesses = []
+        self.guess_count = 0
+        self.win = False
 
     def display(self, display_guess=True):
         if display_guess:
@@ -36,41 +34,65 @@ class Game:
 
     def colourize(self, guess):
         buffer = ""
-        chars = list(self.answer)
-        for x, y in zip(guess, self.answer):
-            if x == y:
-                chars.remove(x)
-        for x, y in zip(guess, self.answer):
-            if x == y:
+        for x, y in self.evaluate(guess):
+            if y == 0:
                 buffer += colour(f" {x} ", GREEN)
-            elif x not in self.answer:
+            elif y == 1:
                 buffer += f" {x} "
             else:
-                if x in chars:
-                    buffer += colour(f" {x} ", YELLOW)
-                    chars.remove(x)
-                else:
-                    buffer += f" {x} "
+                buffer += colour(f" {x} ", YELLOW)
         return buffer
 
+    def evaluate(self, guess):
+        """
+        0 -> GREEN
+        1 -> YELLOW
+        2 -> DEFAULT
+        """
+        evaluation = []
+        chars = [x for x, y in zip(guess, self.answer) if x != y]
+        for x, y in zip(guess, self.answer):
+            if x == y:
+                evaluation.append((x, 0))
+            elif x not in self.answer:
+                evaluation.append((x, 1))
+            else:
+                if x in chars:
+                    evaluation.append((x, 2))
+                    chars.remove(x)
+                else:
+                    evaluation.append((x, 0))
+        return evaluation
+
+    def guess(self, word):
+        if word in self.valid_words:
+            self.guesses.append(word)
+            self.guess_count += 1
+        return word == self.answer
+
     def game_loop(self):
-        while self.guess_count < self.max_guesses:
-            self.display()
-            guess = input()
-            if guess in self.valid_words:
-                self.guesses.append(guess)
-                self.guess_count += 1
-            if guess == self.answer:
-                self.win = True
+        while True:
+            while self.guess_count < self.max_guesses and not self.win:
+                self.display()
+                if self.guess(input()):
+                    self.win = True
                 os.system(CLEAR)
-                break
+
+            self.display(False)
+            if self.win:
+                print(f"You guessed the word in {self.guess_count} guess(es)")
+            else:
+                print(f"The word was {self.answer}")
+
+            input()
+            self.reset()
             os.system(CLEAR)
 
-        self.display(False)
-        if self.win:
-            print(f"You guessed the word in {self.guess_count} guesses.")
-        else:
-            print(f"The word was {self.answer}")
+    def reset(self):
+        self.answer = random.choice(self.valid_answers)
+        self.guesses = []
+        self.guess_count = 0
+        self.win = False
 
 
 def colour(text, c):
@@ -78,7 +100,4 @@ def colour(text, c):
 
 
 if __name__ == "__main__":
-    while True:
-        game = Game()
-        input()
-        os.system(CLEAR)
+    Game().game_loop()
